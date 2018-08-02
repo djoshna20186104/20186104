32ffca52230e4cd6701c50a309593e99d7963a09
djoshna20186104 <djoshna1992@msitprogram.net>
2018-08-02 16:55:43 -0400

    sample

--- cspp1/module3/helloworld.py
+++ cspp1/module3/helloworld.py
@@ -1,1 +1,2 @@
-print('helloworld')
+"'#define helloworld'"
+print("helloworld")
---
+++ cspp1/module3/vowels_counter.py
@@ -0,0 +1,8 @@
+"'#count number of vowels'"
+input_var=input("enter str")
+vowels_str = ""
+for i in vowels_str:
+	if i = 'a' or i = 'e' or i = 'i' or i = 'o' or i = 'u':
+		vowels_str += 1
+print("number of vowels are: ")
+print(vowels)

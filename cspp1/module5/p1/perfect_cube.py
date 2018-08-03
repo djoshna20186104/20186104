99cb1cb4366dd429ec129f7c7ae0abc167b6cce6
djoshna20186104 <djoshna1992@msitprogram.net>
2018-08-03 19:15:28 -0400

    pc

--- cspp1/module5/p1/perfect_cube.py
+++ cspp1/module5/p1/perfect_cube.py
@@ -1,28 +1,24 @@
-# Write a python program to find if the given number is a perfect cube or not
-# using guess and check algorithm
-
+"""# Write a python program to find if the given number is a perfect cube or not
 # testcase 1
 # Input: 24389
 # Output: 24389 is a perfect cube
 
-"'# testcase 2'"
+# testcase 2
 # Input: 21950
-# Output: 21950 is not a perfect cube
-
+# Output: 21950 is not a perfect cube"""
 def main():
-    # input is captured in s
-    # watch out for the data type of value stored in s
-    # your code starts here
-    x = int(input("enter an integer: "))
+    """# input is captured in s
+	# watch out for the data type of value stored in s
+	# your code starts here"""
+    number = int(input())
     ans = 0
-    while ans**3 < abs(x):
+    while ans**3 < abs(number):
         ans = ans + 1
-    if ans**3 != abs(x):
-        print(str(x) + 'is not a perfect cube')
+    if ans**3 != abs(number):
+        print(str(number) + ' is not a perfect cube')
     else:
-        if x < 0:
+        if number < 0:
             ans = - ans
-        print(str(x) + 'is a perfect cube')
-
-if __name__== "__main__":
-	main()
+        print(str(number) + ' is a perfect cube')
+if __name__ == "__main__":
+    main()
